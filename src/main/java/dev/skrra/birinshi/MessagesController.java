a269167270b8f5e625c6dab51031682af0347ab6
skrradev <skrra.dev@gmail.com>
2018-12-03 00:36:23 +0600

    Added get method mapping to fetch all users

--- src/main/java/dev/skrra/birinshi/MessagesController.java
+++ src/main/java/dev/skrra/birinshi/MessagesController.java
@@ -1,7 +1,16 @@
 package dev.skrra.birinshi;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagesController {
+
+
+    @GetMapping("/user/")
+    public String getUsers() {
+
+        return "screw!";
+    }
+
 }
